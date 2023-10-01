f1baf5205a5f5700a5e3ebcdde0e3c83ea6b01d8
Ryan Dosanjh <rndosanj@calpoly.com>
2023-10-01 01:18:21 -0700

    2nd commit

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,14 +1,9 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
-	const [characters, setCharacters] = useState([
-	    {
-	        name: 'Charlie',
-	        job: 'Janitor',
-	         // the rest of the data
-	    },
-	]);  
+	const [characters, setCharacters] = useState([]);  
 
 	function removeOneCharacter (index) {
 	    const updated = characters.filter((character, i) => {
@@ -19,10 +14,11 @@ function MyApp() {
 
   return (
     <div className="container">
-        <Table characterData={characters} 
-	        removeCharacter={removeOneCharacter} />
-    </div>  
-)
+      <Table characterData={characters} 
+        removeCharacter={removeOneCharacter} />
+      <Form />
+    </div>
+  )
 }
 
 export default MyApp;
