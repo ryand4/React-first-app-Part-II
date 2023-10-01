216a86151b47592101a453488be4c0f9d3dab6cd
Ryan Dosanjh <rndosanj@calpoly.com>
2023-09-30 18:31:36 -0700

    your commit msg

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,32 +1,26 @@
-// src/MyApp.js
-import React from "react";
-import Table from "./Table";
+import React, {useState} from 'react';
+import Table from './Table';
 
-export default MyApp;
+function MyApp() {
+	const [characters, setCharacters] = useState([
+	    {
+	        name: 'Charlie',
+	        job: 'Janitor',
+	         // the rest of the data
+	    },
+	]);  
 
-const characters = [
-    {
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ];
-  
-  function MyApp() {
-    return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      );
-  }
+	function removeOneCharacter (index) {
+	    const updated = characters.filter((character, i) => {
+	        return i !== index
+	    });
+	  setCharacters(updated);
+	}
+
+  return (
+    <div className="container">
+        <Table characterData={characters} 
+	        removeCharacter={removeOneCharacter} />
+    </div>  
+)
+}
--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -1,39 +1,14 @@
-//import React from "react";
-
-function TableHeader() {
-  return (
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Job</th>
-      </tr>
-    </thead>
-  );
-}
-
-function TableBody(props) {
-  const rows = props.characterData.map((row, index) => {
+function Table (props) {
     return (
-      <tr key={index}>
+        <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+              <button onClick={() => 
+                  props.removeCharacter(index)}>
+                  Delete
+              </button>
+        </td>
       </tr>
     );
-   }
-  );
-  return (
-      <tbody>
-        {rows}
-       </tbody>
-   );
-}
-
-function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
-}
-export default Table;
+  }
