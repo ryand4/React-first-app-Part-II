94aad73a455984e9e99db8f93aee02e2c3fe8774
ryand4 <97061451+ryand4@users.noreply.github.com>
2023-10-08 23:19:22 -0700

    update list added

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -21,9 +21,13 @@ function MyApp() {
 	  setCharacters(updated);
 	}
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+}
 
   //function submitForm() {
   //  props.handleSubmit(person);
@@ -33,6 +37,19 @@ function MyApp() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
   }
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
   return (
 
     <div className="container">
