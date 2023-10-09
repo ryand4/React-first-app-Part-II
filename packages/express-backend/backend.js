641276a5a0580cc5c27bc4d421786811aad8038c
ryand4 <97061451+ryand4@users.noreply.github.com>
2023-10-08 22:07:18 -0700

    Fixed 404 Error, step 6 done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,7 +3,6 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-
 const users = { 
     users_list : [
        { 
@@ -32,28 +31,26 @@ const users = {
           job: 'Bartender',
        }
     ]
- }
+ };
 
 app.use(express.json());
 
-const findUserByName = (name) => { 
-    return users['users_list']
-        .filter( (user) => user['name'] === name); 
-}
+const findUserByName = (name) => {
+    return users['users_list'].filter( (user) => user['name'] === name);
+};
 
-const findUserById = (id) =>
-    users['users_list']
-        .find( (user) => user['id'] === id);
+const findUserById = (id) => 
+    users['users_list'].find( (user) => user['id'] === id);
 
-        app.get('/users/:id', (req, res) => {
-            const id = req.params['id']; //or req.params.id
-            let result = findUserById(id);
-            if (result === undefined) {
-                res.status(404).send('Resource not found.');
-            } else {
-                res.send(result);
-            }
-        });
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id'];
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+})
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
@@ -61,12 +58,27 @@ app.get('/users', (req, res) => {
         let result = findUserByName(name);
         result = {users_list: result};
         res.send(result);
-    }
-    else{
+    } else {
         res.send(users);
     }
 });
 
-app.listen(port, () => {
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+})
+
+app.listen(port, ()=>{
     console.log(`Example app listening at http://localhost:${port}`);
-}); 
+});
